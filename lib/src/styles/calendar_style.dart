db1ad2c42cbb698d77b05faf42d1ac3b76c46da7
alvin <phuong.vu.dev@gmail.com>
2025-02-18 16:04:15 +0700

    Refactor: Update package metadata and remove unused calendar style
    - Update package description in `pubspec.yaml` to "A simple Flutter package for displaying a calendar.".
    - Add package homepage link to `pubspec.yaml`.
    - Remove unused `CalendarStyle` class and its export from `ui_simple_calendar.dart`.

--- lib/src/styles/calendar_style.dart
+++
@@ -1,5 +0,0 @@
-import 'dart:ui';
-
-final class CalendarStyle {
-
-}
--- lib/ui_simple_calendar.dart
+++ lib/ui_simple_calendar.dart
@@ -1,5 +1,4 @@
 library ui_simple_calendar;
 
 export 'src/calendar.dart';
-export 'src/styles/calendar_style.dart';
 export 'src/controller/calendar_controller.dart';
